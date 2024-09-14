1090fd8c2e0cea0135f1ff7fac52499702834bed
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-14 04:15:57 -0400

    Update AdminFactory.php

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -7,12 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class AdminFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Admin::class;
+
 
     /**
      * Define the model's default state.
