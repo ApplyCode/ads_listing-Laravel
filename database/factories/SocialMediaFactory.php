68d4d7814528f1d77bc12a58d87988452d626180
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-14 04:22:01 -0400

    Update SocialMediaFactory.php

--- database/factories/SocialMediaFactory.php
+++ database/factories/SocialMediaFactory.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class SocialMediaFactory extends Factory
 {
-
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
     public function definition()
     {
         return [
