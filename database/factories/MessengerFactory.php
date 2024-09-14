b270e2e3cebd0b41186f006df64055cbdb76c193
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-14 04:18:39 -0400

    Update MessengerFactory.php

--- database/factories/MessengerFactory.php
+++ database/factories/MessengerFactory.php
@@ -2,9 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\MessengerUser;
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Arr;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Messenger>
